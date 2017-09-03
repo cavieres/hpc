3a1fbccd8a5b33253092b752693532f63f34013d
cavieres <cavieres.ruben@gmail.com>
2017-09-03 17:30:48 -0300

    in-register sorting until transposing matrix.

--- simdsort.c
+++ simdsort.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 #include <pmmintrin.h>
 #include <string.h>
-#define TOTAL_SECUENCES 10
+#define TOTAL_SECUENCES 16
 #define SUB_SECUENCES 4
 
-__m128 orderedSubSec1, orderedSubSec2;
+__m128 bmnSubSec1, bmnSubSec2;
+__m128 inRegSubSec1, inRegSubSec2, inRegSubSec3, inRegSubSec4;
 
 float *getSecuences(char *filename)
 {
 	//static float secuences[TOTAL_SECUENCES] = {-5459555, -1419643, 9206201, 4325544, 7233019, -7826876, -5901765, 1576008, 6165429, -2737032};
 	//static float secuences[TOTAL_SECUENCES] = {1,2,3,4, 8,7,6,5, 30, 45};
-	static float secuences[TOTAL_SECUENCES] = {5,20,25,30, 18,9,7,2, 30, 45};
+	//static float secuences[TOTAL_SECUENCES] = {5,20,25,30, 18,9,7,2, 30, 45};
+	static float secuences[TOTAL_SECUENCES] = {12,21,4,13, 9,8,6,7,1,14,3,0,5,11,15,10};
 	return secuences;
-}
-
-int min()
-{
-}
-
-int max()
-{}
-
-int shuffle()
-{}
-
-int sortSecuencesAsc(int sec1, int sec2, int sec3, int sec4)
-{
-	min();
-	max(); 
-	shuffle();
 }
 
 __m128 arrangeBmnSubSecs(__m128 subSec1, __m128 subSec2, int subSecNumber)
@@ -55,11 +40,6 @@ __m128 arrangeBmnSubSecs(__m128 subSec1, __m128 subSec2, int subSecNumber)
 	return arrangedSubSecFinal;
 }
 
-__m128 arrangeBmnOrderedSubSecs(__m128 orderedSubSec1, __m128 orderedSubSec2, int subSecNumber)
-{
-	
-}
-
 void bmn(__m128 subSec1, __m128 subSec2)
 {
 	// Get two 4-lenght ascending ordered secuences.
@@ -81,8 +61,8 @@ void bmn(__m128 subSec1, __m128 subSec2)
 		i++;
 	}
 	
-	orderedSubSec1 = subSecMin;
-	orderedSubSec2 = subSecMax;
+	bmnSubSec1 = subSecMin;
+	bmnSubSec2 = subSecMax;
 	
 	printf("subSecMin: %f %f %f %f\n", subSecMin[0], subSecMin[1], subSecMin[2], subSecMin[3]);
 	printf("subSecMax: %f %f %f %f\n", subSecMax[0], subSecMax[1], subSecMax[2], subSecMax[3]);
@@ -94,6 +74,61 @@ int mergeSimd(int secuence)
 	// Get a secuence lenght 16.  
 }
 
+void transposeRegisterMatrix(__m128 subSec1, __m128 subSec2, __m128 subSec3, __m128 subSec4)
+{
+	__m128 arranged1 = arrangeBmnSubSecs(subSec1, subSec2, 1);
+	__m128 arranged2 = arrangeBmnSubSecs(subSec3, subSec4, 1);
+	__m128 arranged3 = arrangeBmnSubSecs(subSec1, subSec2, 2);
+	__m128 arranged4 = arrangeBmnSubSecs(subSec3, subSec4, 2);
+	
+	inRegSubSec1 = _mm_shuffle_ps(arranged1, arranged2, _MM_SHUFFLE(1, 0, 1, 0));
+	inRegSubSec2 = _mm_shuffle_ps(arranged1, arranged2, _MM_SHUFFLE(3, 2, 3, 2));
+	inRegSubSec3 = _mm_shuffle_ps(arranged3, arranged4, _MM_SHUFFLE(1, 0, 1, 0));
+	inRegSubSec4 = _mm_shuffle_ps(arranged3, arranged4, _MM_SHUFFLE(3, 2, 3, 2));
+	
+	printf("inRegSubSec1: %f %f %f %f\n", inRegSubSec1[0], inRegSubSec1[1], inRegSubSec1[2], inRegSubSec1[3]);
+	printf("inRegSubSec2: %f %f %f %f\n", inRegSubSec2[0], inRegSubSec2[1], inRegSubSec2[2], inRegSubSec2[3]);
+	printf("inRegSubSec3: %f %f %f %f\n", inRegSubSec3[0], inRegSubSec3[1], inRegSubSec3[2], inRegSubSec3[3]);
+	printf("inRegSubSec4: %f %f %f %f\n", inRegSubSec4[0], inRegSubSec4[1], inRegSubSec4[2], inRegSubSec4[3]);
+}
+
+void inRegisterSorting(__m128 subSec1, __m128 subSec2, __m128 subSec3, __m128 subSec4)
+{
+	__m128 subSecAux;
+	
+	subSecAux = _mm_min_ps(subSec1, subSec2);
+	subSec2 = _mm_max_ps(subSec1, subSec2);
+	subSec1 = subSecAux;
+	
+	subSecAux = _mm_min_ps(subSec3, subSec4);
+	subSec4 = _mm_max_ps(subSec3, subSec4);
+	subSec3 = subSecAux;
+	
+	subSecAux = _mm_min_ps(subSec1, subSec3);
+	subSec3 = _mm_max_ps(subSec1, subSec3);
+	subSec1 = subSecAux;
+	
+	subSecAux = _mm_min_ps(subSec2, subSec4);
+	subSec4 = _mm_max_ps(subSec2, subSec4);
+	subSec2 = subSecAux;
+	
+	subSecAux = _mm_min_ps(subSec1, subSec4);
+	subSec4 = _mm_max_ps(subSec1, subSec4);
+	subSec1 = subSecAux;
+	
+	subSecAux = _mm_min_ps(subSec2, subSec3);
+	subSec3 = _mm_max_ps(subSec2, subSec3);
+	subSec2 = subSecAux;
+
+	printf("subSec1: %f %f %f %f\n", subSec1[0], subSec1[1], subSec1[2], subSec1[3]);
+	printf("subSec2: %f %f %f %f\n", subSec2[0], subSec2[1], subSec2[2], subSec2[3]);
+	printf("subSec3: %f %f %f %f\n", subSec3[0], subSec3[1], subSec3[2], subSec3[3]);
+	printf("subSec4: %f %f %f %f\n", subSec4[0], subSec4[1], subSec4[2], subSec4[3]);
+	
+	transposeRegisterMatrix(subSec1, subSec2, subSec3, subSec4);
+	//inRegSubSec1 = subSec1;
+}
+
 int main()
 {
 	printf("LAB1: SIMD-SSE\n");
@@ -103,22 +138,34 @@ int main()
 
 	sec = getSecuences(filename);
 
-	__m128 A1, A2;
+	__m128 A1, A2, A3, A4;
 	float a1[4] __attribute__((aligned(16)));
 	float a2[4] __attribute__((aligned(16)));
+	float a3[4] __attribute__((aligned(16)));
+	float a4[4] __attribute__((aligned(16)));
 
 	memcpy(a1, (sec + 0), SUB_SECUENCES * 4);
 	memcpy(a2, (sec + 4), SUB_SECUENCES * 4);
+	memcpy(a3, (sec + 8), SUB_SECUENCES * 4);
+	memcpy(a4, (sec + 12), SUB_SECUENCES * 4);
 
 	A1 = _mm_load_ps(a1);
 	A2 = _mm_load_ps(a2);
+	A3 = _mm_load_ps(a3);
+	A4 = _mm_load_ps(a4);
+
+	inRegisterSorting(A1, A2, A3, A4);
 
 	//__m128 *res = bmn(A1, A2);
-	bmn(A1, A2);
+	//bmn(A1, A2);
+	
+	
 	
 	printf("A1: %f %f %f %f\n", A1[0], A1[1], A1[2], A1[3]);
 	printf("A2: %f %f %f %f\n", A2[0], A2[1], A2[2], A2[3]);
-	printf("orderedSubSec1: %f %f %f %f\n", orderedSubSec1[0], orderedSubSec1[1], orderedSubSec1[2], orderedSubSec1[3]);
+	printf("A3: %f %f %f %f\n", A3[0], A3[1], A3[2], A3[3]);
+	printf("A4: %f %f %f %f\n", A4[0], A4[1], A4[2], A4[3]);
+	//printf("bmnSubSec1: %f %f %f %f\n", bmnSubSec1[0], bmnSubSec1[1], bmnSubSec1[2], bmnSubSec1[3]);
 	
 	return 0;
 }

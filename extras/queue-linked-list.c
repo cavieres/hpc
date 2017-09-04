1080cb2ea71430cdb17f1bc986c89cf95eba3aa4
cavieres <cavieres.ruben@gmail.com>
2017-09-04 17:49:57 -0300

    Some examples to build queues in ANSI C.

---
+++ extras/queue-linked-list.c
@@ -0,0 +1,16 @@
+
+// https://gist.github.com/mycodeschool/7510222
+#include <stdio.h>
+
+struct node {
+	float data;
+	struct node *next;
+};
+
+int main()
+{
+	struct node * n1 = NULL;
+	
+	
+	return 0;
+}

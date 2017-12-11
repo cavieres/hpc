62bcfd7b84f7a70edefce6b69c006e65b07688ac
Your Name <you@example.com>
2017-12-11 13:43:20 -0300

    Modifyng threads in benchmark.

--- lab-4/lab4-benchmark.sh
+++ lab-4/lab4-benchmark.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-#echo N\;X\;Y\;T\;Time\;
+echo np\;r\;c\;i\;s\;Time\;
 
 mpicc gol.c -fopenmp -std=c99 -o gol
 
-mpirun -np 32 ./gol -r 128 -c 128 -i 150 -m 0 -t 10 -s 100
-mpirun -np 64 ./gol -r 128 -c 128 -i 150 -m 0 -t 10 -s 100
-mpirun -np 128 ./gol -r 128 -c 128 -i 150 -m 0 -t 10 -s 100
-mpirun -np 256 ./gol -r 128 -c 128 -i 150 -m 0 -t 10 -s 100
+mpirun -np 32 ./gol -r 128 -c 128 -i 150 -m 0 -t 1 -s 100
+mpirun -np 64 ./gol -r 128 -c 128 -i 150 -m 0 -t 1 -s 100
+mpirun -np 128 ./gol -r 128 -c 128 -i 150 -m 0 -t 1 -s 100
 
-mpirun -np 32 ./gol -r 256 -c 256 -i 150 -m 0 -t 10 -s 100
-mpirun -np 64 ./gol -r 256 -c 256 -i 150 -m 0 -t 10 -s 100
-mpirun -np 128 ./gol -r 256 -c 256 -i 150 -m 0 -t 10 -s 100
-mpirun -np 256 ./gol -r 256 -c 256 -i 150 -m 0 -t 10 -s 100
+mpirun -np 32 ./gol -r 256 -c 256 -i 150 -m 0 -t 1 -s 100
+mpirun -np 64 ./gol -r 256 -c 256 -i 150 -m 0 -t 1 -s 100
+mpirun -np 128 ./gol -r 256 -c 256 -i 150 -m 0 -t 1 -s 100
 
-mpirun -np 32 ./gol -r 512 -c 512 -i 150 -m 0 -t 10 -s 100
-mpirun -np 64 ./gol -r 512 -c 512 -i 150 -m 0 -t 10 -s 100
-mpirun -np 128 ./gol -r 512 -c 512 -i 150 -m 0 -t 10 -s 100
-mpirun -np 256 ./gol -r 512 -c 512 -i 150 -m 0 -t 10 -s 100
+mpirun -np 32 ./gol -r 512 -c 512 -i 150 -m 0 -t 1 -s 100
+mpirun -np 64 ./gol -r 512 -c 512 -i 150 -m 0 -t 1 -s 100
+mpirun -np 128 ./gol -r 512 -c 512 -i 150 -m 0 -t 1 -s 100
 
-mpirun -np 32 ./gol -r 128 -c 128 -i 300 -m 0 -t 10 -s 100
-mpirun -np 64 ./gol -r 128 -c 128 -i 300 -m 0 -t 10 -s 100
-mpirun -np 128 ./gol -r 128 -c 128 -i 300 -m 0 -t 10 -s 100
-mpirun -np 256 ./gol -r 128 -c 128 -i 300 -m 0 -t 10 -s 100
+mpirun -np 32 ./gol -r 128 -c 128 -i 300 -m 0 -t 1 -s 100
+mpirun -np 64 ./gol -r 128 -c 128 -i 300 -m 0 -t 1 -s 100
+mpirun -np 128 ./gol -r 128 -c 128 -i 300 -m 0 -t 1 -s 100
 
-mpirun -np 32 ./gol -r 256 -c 256 -i 300 -m 0 -t 10 -s 100
-mpirun -np 64 ./gol -r 256 -c 256 -i 300 -m 0 -t 10 -s 100
-mpirun -np 128 ./gol -r 256 -c 256 -i 300 -m 0 -t 10 -s 100
-mpirun -np 256 ./gol -r 256 -c 256 -i 300 -m 0 -t 10 -s 100
+mpirun -np 32 ./gol -r 256 -c 256 -i 300 -m 0 -t 1 -s 100
+mpirun -np 64 ./gol -r 256 -c 256 -i 300 -m 0 -t 1 -s 100
+mpirun -np 128 ./gol -r 256 -c 256 -i 300 -m 0 -t 1 -s 100
 
-mpirun -np 32 ./gol -r 512 -c 512 -i 300 -m 0 -t 10 -s 100
-mpirun -np 64 ./gol -r 512 -c 512 -i 300 -m 0 -t 10 -s 100
-mpirun -np 128 ./gol -r 512 -c 512 -i 300 -m 0 -t 10 -s 100
-mpirun -np 256 ./gol -r 512 -c 512 -i 300 -m 0 -t 10 -s 100
+mpirun -np 32 ./gol -r 512 -c 512 -i 300 -m 0 -t 1 -s 100
+mpirun -np 64 ./gol -r 512 -c 512 -i 300 -m 0 -t 1 -s 100
+mpirun -np 128 ./gol -r 512 -c 512 -i 300 -m 0 -t 1 -s 100
 
 
